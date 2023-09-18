60c2a33ea043899a11829cf8e1ff935260b67ea6
JF-PS <jfps.dev21@gmail.com>
2023-09-18 11:41:00 +0200

    tets

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    // allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/module/profile.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { TokenService } from '../service/token.service';
+import { PrismaService } from '../service/prisma.service';
+import { StripeService } from '../service/stripe.service';
+import { PublicTokenGuard } from '../guard/publicToken.guard';
+import { ProfileResolver } from '../resolver/profile.resolver';
+
+import { AuthRepository } from '../repository/auth.repository';
+import { ProfileBusiness } from '../business/profile.business';
+import { ProfileRepository } from '../repository/profile.repository';
+import { PublicTokenStrategy } from '../strategy/publicToken.strategy';
+import { RefreshTokenStrategy } from '../strategy/refreshToken.strategy';
+import { GeospatialService } from '../service/spot-geospatial.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SpotSchema } from '../schema/spot.shema';
+import { ProfileSchema } from '../schema/profile.shema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Spot', schema: SpotSchema },
+      { name: 'Profile', schema: ProfileSchema },
+    ]),
+  ],
+  providers: [
+    ProfileResolver,
+    ProfileRepository,
+    ProfileBusiness,
+    AuthRepository,
+    JwtService,
+    TokenService,
+    StripeService,
+    PrismaService,
+    PublicTokenGuard,
+    PublicTokenStrategy,
+    RefreshTokenStrategy,
+    GeospatialService,
+  ],
+})
+export class ProfileModule {}
